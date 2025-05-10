6fcb30a4c09c4fe10237b433da6d233e2f659c0b
AndrejLapin <lapin.andrej1@gmail.com>
2025-05-10 14:28:13 +0300

    better movement

--- scripts/components/ship_movement.gd
+++ scripts/components/ship_movement.gd
@@ -3,7 +3,7 @@ class_name ShipMovement
 extends Component
 
 @export var acceleration: float = 100
-@export var turn_acceleration: float = 10
+@export var turn_acceleration: float = 50
 @export var max_turn_speed: float = 2.0
 
 var _parent: RigidBody3D = null
@@ -27,8 +27,15 @@ func _physics_process(delta: float) -> void:
 		_parent.apply_central_force(acceleration_direction * acceleration * _parent.mass)
 	
 	if turn_input:
-		var inverse_intertia := PhysicsServer3D.body_get_direct_state(_parent.get_rid()).inverse_inertia.y
+		var inverse_inertia := PhysicsServer3D.body_get_direct_state(_parent.get_rid()).inverse_inertia.y
 		var acceleration_defficit := (max_turn_speed - absf(_parent.angular_velocity.y)) / delta
-		var torque_to_apply: float = turn_input * clamp(turn_acceleration, -acceleration_defficit, acceleration_defficit) / inverse_intertia
+		var torque_to_apply: float = turn_input * clamp(turn_acceleration, -acceleration_defficit, acceleration_defficit) / inverse_inertia
 		_parent.apply_torque(Vector3(0.0, torque_to_apply, 0.0))
+	else:
+		var inverse_inertia := PhysicsServer3D.body_get_direct_state(_parent.get_rid()).inverse_inertia.y
+		if inverse_inertia != 0:
+			var reverse_angular_acceleration := -_parent.angular_velocity.y / delta as float
+			reverse_angular_acceleration = clamp(reverse_angular_acceleration, -turn_acceleration, turn_acceleration)
+			var torque_to_apply: float = reverse_angular_acceleration / inverse_inertia
+			_parent.apply_torque(Vector3(0.0, torque_to_apply, 0.0))
 	#_input_polling_component.get_turn()
