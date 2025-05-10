de7e3103942e25ae671737f12e23814ae873c3fa
AndrejLapin <lapin.andrej1@gmail.com>
2025-05-10 14:16:51 +0300

    basic controlls and terrain

---
+++ scripts/components/ship_movement.gd
@@ -0,0 +1,34 @@
+## TODO: Add short component description
+class_name ShipMovement
+extends Component
+
+@export var acceleration: float = 100
+@export var turn_acceleration: float = 10
+@export var max_turn_speed: float = 2.0
+
+var _parent: RigidBody3D = null
+var _input_polling_component: IInputPollingComponent = null
+
+static func core() -> ComponentCore:
+	return ComponentCore.new(ShipMovement)
+
+
+func _ready() -> void:
+	_parent = get_parent()
+	_input_polling_component = IInputPollingComponent.core().get_of_type_from(_parent)
+
+
+func _physics_process(delta: float) -> void:
+	var acceleration_input := _input_polling_component.get_acceleration()
+	var turn_input := _input_polling_component.get_turn() * -1.0
+	
+	if acceleration_input:
+		var acceleration_direction := _parent.transform.basis * Vector3(0.0, 0.0, acceleration_input)
+		_parent.apply_central_force(acceleration_direction * acceleration * _parent.mass)
+	
+	if turn_input:
+		var inverse_intertia := PhysicsServer3D.body_get_direct_state(_parent.get_rid()).inverse_inertia.y
+		var acceleration_defficit := (max_turn_speed - absf(_parent.angular_velocity.y)) / delta
+		var torque_to_apply: float = turn_input * clamp(turn_acceleration, -acceleration_defficit, acceleration_defficit) / inverse_intertia
+		_parent.apply_torque(Vector3(0.0, torque_to_apply, 0.0))
+	#_input_polling_component.get_turn()
