490324a1366a267fa184d3e664cc6aaf09494d44
AndrejLapin <lapin.andrej1@gmail.com>
2025-05-11 13:49:08 +0300

    Crew UI improvements

--- scripts/ui/stat_ui.gd
+++ scripts/ui/stat_ui.gd
@@ -8,11 +8,16 @@ extends HBoxContainer
 			stat_name_label.text = ""
 		if is_node_ready():
 			stat_name_label.text = value
-@export var bar_style_texture: StyleBoxTexture = load("res://resources/ui/white_bar.tres"):
+@export var bar_color: Color = Color.WHITE:
 	set(value):
-		bar_style_texture = value
+		bar_color = value
 		if is_node_ready():
-			stat_representation_bar.add_theme_stylebox_override("fill", value)
+			stat_representation_bar.modulate = value
+#@export var bar_style_texture: StyleBoxTexture = load("res://resources/ui/white_bar.tres"):
+	#set(value):
+		#bar_style_texture = value
+		#if is_node_ready():
+			#stat_representation_bar.add_theme_stylebox_override("fill", value)
 @export var bar_fill_mode: ProgressBar.FillMode = ProgressBar.FillMode.FILL_BEGIN_TO_END:
 	set(value):
 		if value == null:
@@ -26,5 +31,6 @@ extends HBoxContainer
 
 func _ready() -> void:
 	stat_name_label.text = stat_name
-	stat_representation_bar.add_theme_stylebox_override("fill", bar_style_texture)
+	stat_representation_bar.modulate = bar_color
 	stat_representation_bar.fill_mode = bar_fill_mode
+	#stat_representation_bar.add_theme_stylebox_override("fill", bar_style_texture)
